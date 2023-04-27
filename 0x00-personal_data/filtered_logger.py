6784e29b15df7eaa3f10406af4f9623f6ee41edb
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-27 04:35:51 -0400

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
-"""A module for filtering logs.
+""" 0x05. Personal data
+How to implement a log filter that will obfuscate PII fields
+How to encrypt a password and check the validity
+How to authenticate to a database using environment variables
 """
 import os
 import re
-import logging
 import mysql.connector
 from typing import List
 
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str,
-        ) -> str:
-    """Filters a log line.
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ a function called filter_datum that returns the log message
+        Args:
+            fields: a list of strings representing all fields
+            redaction: a string representing by what the field will be obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is separating all 
+            fields in the log line (message) 
+        Returns: message
     """
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+    for format in fields:
+        message = re.sub(rf"{format}=(.*?)\{separator}",
+                         f'{format}={redaction}{separator}', message)
+    return message
