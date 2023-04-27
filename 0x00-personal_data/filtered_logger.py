f2c94a0731a5586a1e028a4f819855e9f95602a1
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-27 04:28:33 -0400

    filtered logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" 0x05. Personal data
+
+"""
+
+import logging
+import os
+import re
+import mysql.connector
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ a function called filter_datum that returns the log message obfuscated:
+        Args:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: a string representing by what the field will be obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is separating all fields in the log line (message) 
+        Returns: message
+    """
+    for format in fields:
+        message = re.sub(rf"{format}=(.*?)\{separator}",
+                         f'{format}={redaction}{separator}', message)
+    return message
