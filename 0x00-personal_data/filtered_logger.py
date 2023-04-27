35df6eeb265a75caf4c34b8a30fb18c05703b28b
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-27 04:32:50 -0400

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ 0x05. Personal data
-
+How to implement a log filter that will obfuscate PII fields
+How to encrypt a password and check the validity of an input password
+How to authenticate to a database using environment variables
 """
-
-import logging
 import os
 import re
 import mysql.connector
