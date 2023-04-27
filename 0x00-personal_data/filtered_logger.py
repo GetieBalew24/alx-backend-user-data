35a42ca66d7c0fec055a151fc6f024b3d1f9f191
GetieBalew24 <getiebalew2020@gmail.com>
2023-04-27 04:33:49 -0400

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ 0x05. Personal data
 How to implement a log filter that will obfuscate PII fields
-How to encrypt a password and check the validity of an input password
+How to encrypt a password and check the validity
 How to authenticate to a database using environment variables
 """
 import os
@@ -13,11 +13,10 @@ from typing import List
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str, message: 
-    str, separator: str) -> str:
-    """ a function called filter_datum that returns the log message obfuscated:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ a function called filter_datum that returns the log message
         Args:
-            fields: a list of strings representing all fields to obfuscate
+            fields: a list of strings representing all fields
             redaction: a string representing by what the field will be obfuscated
             message: a string representing the log line
             separator: a string representing by which character is separating all 
