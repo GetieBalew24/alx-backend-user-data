b6bcab2da7024e3c4a6e41e30e8d89d55f020cdb
GetieBalew24 <getiebalew2020@gmail.com>
2023-05-11 11:22:22 -0400

    database tabele name users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
-""" create a SQLAlchemy model named User for a database table named users
+"""The `user` model's module.
 """
-
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class User(Base):
+    """Represents a record from the `user` table.
     """
-    class user 
-    """
-
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        """
-        String representation.
-        """
-        return f"User: id={self.id}"
